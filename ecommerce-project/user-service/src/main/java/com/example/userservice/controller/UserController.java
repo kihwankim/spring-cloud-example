49dd7d8242f0af75655bb01863dd429b9d8238d8
kihwankim <clack2933@gmail.com>
2021-06-15 20:25:49 +0900

    refactor: yml 파일로 코드 모듈화

--- ecommerce-project/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ ecommerce-project/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.vo.Greeting;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final Environment env;
+    private final Greeting greeting;
 
     @GetMapping("/health-check")
     public String status() {
@@ -20,6 +20,6 @@ public class UserController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return env.getProperty("greeting.message");
+        return greeting.getMessage();
     }
 }
