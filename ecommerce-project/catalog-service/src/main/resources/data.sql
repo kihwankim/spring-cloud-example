567c055cd0dad4c63d8c29c876844efbb1f414b9
kihwankim <clack2933@gmail.com>
2021-06-17 10:20:08 +0900

    feat: catalog project init

---
+++ ecommerce-project/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO catalog(product_id, product_name, stock, unit_price) VALUES('CATALOG-001', 'Berlin', 100, 1500),
+                                                                       ('CATALOG-002', 'Tokyo', 100, 900),
+                                                                       ('CATALOG-003', 'Stockholm', 100, 1200)
